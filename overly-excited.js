5d3207da401e13301318ec39867736876d8ab7e0
Eli Jackson <EliJackson@elis-mbp.attlocal.net>
2017-11-28 21:38:06 -0600

    updated JS for first 2 parts of the assignment

--- overly-excited.js
+++ overly-excited.js
@@ -1,12 +1,26 @@
-var sentence = ["The","walrus","danced","through","the","trees","in","the","light", "of", "the", "moon"]
+var sentence = ["The","walrus","danced","through","the","trees","in","the","light", "of", "the", "moon"];
 
 
 // The addExcitement function should accept the array as the sole argument
-function addExcitement (theWordArray) {
-    for (var i=0; i<sentence.length; i++) {}
-}
+// function addExcitement (sentence) {
+//     var complete = "";
+//     for (var i=0; i<sentence.length; i++) {
+//         complete = complete + " " + sentence[i];
+//         console.log(complete);
+//     }
+// };
 
-// Invoke the function and pass in the array
-function addExcitement(sentence) {
+// // Invoke the function and pass in the array
+function addExcitement (sentence) {
+    var complete = "";
+    for (var i = 0; i<sentence.length; i+=1) {
+        if ((i+1)%3 === 0) {
+            complete += sentence[i] + "! ";
+            console.log(complete);
+        } else {
+            complete += sentence[i] + " ";
+            console.log(complete);
+        } 
+    }
+};
 
-}

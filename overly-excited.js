df01d8f7058687628d8d23bfe12f7f13cacdf0b5
Eli Jackson <EliJackson@elis-mbp.attlocal.net>
2017-11-28 21:20:00 -0600

    adding JS info

---
+++ overly-excited.js
@@ -0,0 +1,12 @@
+var sentence = ["The","walrus","danced","through","the","trees","in","the","light", "of", "the", "moon"]
+
+
+// The addExcitement function should accept the array as the sole argument
+function addExcitement (theWordArray) {
+    for (var i=0; i<sentence.length; i++) {}
+}
+
+// Invoke the function and pass in the array
+function addExcitement(sentence) {
+
+}
